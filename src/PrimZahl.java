903c1234520c5e252e8e87c18eb81a77bfb3f957
MaxAhnfeldt <maximilian.ahnfeldt@nordakademie.de>
2016-11-02 13:27:40 +0100

    blabla

--- src/PrimZahl.java
+++ src/PrimZahl.java
@@ -3,36 +3,40 @@ public class PrimZahl {
 
 	public static void main(String[] args) {
 
-		int testzahl = 2;
-		boolean pruefung1;
-		pruefung1 = isPrimzahl(testzahl);
+		int testzahl = 4;
+		boolean pruefung1 = true;
 
-		if (pruefung1==true) {
-			System.out.println("Die Zahl ist eine Primzahl!");
+		if (testzahl == 1) {
+			System.out.println(testzahl + " ist keine Primzahl!");
+		} else if (testzahl == 2) {
+			System.out.println(testzahl + " ist eine Primzahl!");
 		} else {
-			System.out.println("Die Zahl ist keine Primzahl!");
-		}
 
+			pruefung1 = isPrimzahl(testzahl);
+
+			if (pruefung1 == true) {
+				System.out.println(testzahl + " ist eine Primzahl!");
+			} else {
+				System.out.println(testzahl + " ist keine Primzahl!");
+			}
+		}
 	}
 
-	public static boolean isPrimzahl(int testzahl) {
-		boolean pruefung;
+	public static boolean isPrimzahl(int testzahl1) {
+
 		int zahl = 2;
+		boolean pruefung = true;
 
-		while (zahl < testzahl) {
-			zahl++;
+		while (zahl < testzahl1) {
 
-			if (testzahl == 1) {
-				pruefung = false;
-			}
-
-			if (testzahl % zahl == 0) {
+			if (testzahl1 % zahl == 0) {
 				pruefung = false;
 			} else {
 				pruefung = true;
 			}
 
-			return (pruefung);
+			zahl=zahl+1;
 		}
+		return pruefung;
 	}
 }
